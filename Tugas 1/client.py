0d1ffc1df995887682c1d69ad7fc63bf305bbb11
Yasinta Romadhona <yasinta.roro@gmail.com>
2019-02-21 18:10:33 +0700

    assigned

---
+++ Tugas 1/client.py
@@ -0,0 +1,28 @@
+import sys
+import socket
+
+#create a tcp/ip socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect the socket to the port where the server is listening
+server_address = ('localhost',10000)
+print >> sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
+
+try:
+    #send data
+    message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHJKILMNOPQ'
+    print >> sys.stderr, 'sending "%s"' % message
+    sock.sendall(message)
+
+    #look for the response
+    amount_received = 0
+    amount_expected = len(message)
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print >> sys.stderr, 'received "%s"' % data
+
+finally:
+    print >> sys.stderr, 'closing socket'
+    sock.close()
---
+++ Tugas 1/server.py
@@ -0,0 +1,31 @@
+import sys
+import socket
+
+#create a tcp/ip socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#bind the socket to the port
+server_address = ('localhost',10000)
+print >> sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+
+#listen for incoming connections
+sock.listen(1)
+while True:
+    #wait for a connection
+    print >> sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+    print >> sys.stderr, 'connection form', client_address
+
+    #receive the data in small chunks and retransmit it
+    while True:
+        data = connection.recv(32)
+        print >> sys.stderr, 'received "%s"' % data
+        if data:
+            print >> sys.stderr, 'sending data back to the client'
+            connection.sendall('-->'+data)
+        else:
+            print >> sys.stderr, 'no more data from', client_address
+            break
+    #clean up the connection
+    connection.close()
