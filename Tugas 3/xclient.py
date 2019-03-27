2e22c84004f06c7841c76bb867e38d92a13fd68a
Yasinta Romadhona <yasinta.roro@gmail.com>
2019-03-27 16:43:53 +0700

    assigned

---
+++ Tugas 3/xclient.py
@@ -0,0 +1,43 @@
+import socket
+
+def Main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.connect((host, port))
+
+    filename = raw_input("Input File? -> ")
+    if filename[:4] == 'list':
+        s.send(filename)
+        inputFile = s.recv(1024)
+        print "directory: " + str(inputFile)
+
+    else:
+        s.send(filename)
+    data = s.recv(1024)
+    if data[:6] == 'EXISTS':
+        filesize = long(data[6:])
+        notif = raw_input("File exists, " + str(filesize) + " Bytes, Download? (Y/N) -> ")
+        if notif == 'Y':
+            s.send("OK")
+            new_filename = filename.rsplit("/")[-1]
+            f = open('new ' + new_filename, 'wb')
+            data = s.recv(1024)
+            totalRecv = len(data)
+            f.write(data)
+            while totalRecv < filesize:
+                data = s.recv(1024)
+                totalRecv += len(data)
+                f.write(data)
+                print "{0:.2f}".format((totalRecv / float(filesize)) * 100) + "% Done"
+
+            print "Completed!"
+            f.close()
+    else:
+        print "Not Exist!"
+    s.close()
+
+
+if __name__ == '__main__':
+    Main()
---
+++ Tugas 3/xserver.py
@@ -0,0 +1,55 @@
+import socket
+import threading
+import os
+
+def Process(name, sock):
+    filename = sock.recv(1024)
+
+    if filename[:4] == 'list':  # list directory
+        print "directory list"
+        print "path: " + filename[5:]
+        filelist = os.listdir(filename[5:])
+        print filelist
+        sock.send(str(filelist))
+
+    elif os.path.isfile(filename):  # list to download image
+        sock.send("EXISTS " + str(os.path.getsize(filename)))
+        userResponse = sock.recv(1024)
+        if userResponse[:2] == 'OK':
+            with open(filename, 'rb') as f:
+                totalBytes = f.read(1024)
+                sock.send(totalBytes)
+                while totalBytes != "":
+                    totalBytes = f.read(1024)
+                    sock.send(totalBytes)
+        else:
+            sock.send("Error.")
+
+
+    else:
+        print "Error."
+
+    sock.close()
+
+
+def Main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+
+    s.listen(5)
+
+    print "Server is ready."
+    while True:
+        c, addr = s.accept()
+        print "client connected to " + str(addr)
+        t = threading.Thread(target=Process, args=("retrThread", c))
+        t.start()
+
+    s.close()
+
+
+if __name__ == '__main__':
+    Main()
